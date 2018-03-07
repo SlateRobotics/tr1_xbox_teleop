41c93f9702702a6b045947e9a1a48758cdb4e767
Zach417 <zach@slaterobots.com>
2018-03-06 20:56:46 -0600

    added publisher

--- src/tr1_xbox_controller_node.py
+++ src/tr1_xbox_controller_node.py
@@ -2,13 +2,18 @@
 
 import rospy
 from sensor_msgs.msg import Joy
+from std_msgs.msg import Float64
 
-def callback(data):
-	rospy.loginfo(data)
+pub = rospy.Publisher('/tr1/arm1_to_arm2_joint_effort_controller/command', Float64, queue_size=10)
+
+def subscriber_callback(data):
+	joint1_effort = data.axes[0]
+	rospy.loginfo(joint1_effort)
+	pub.publish(joint1_effort)
 
 def do_control():
 	rospy.init_node('tr1_xbox_controller', anonymous=True)
-	rospy.Subscriber("joy", Joy, callback)
+	rospy.Subscriber("joy", Joy, subscriber_callback)
 	rospy.spin()
 
 if __name__ == '__main__':
