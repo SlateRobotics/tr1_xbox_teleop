2eef6781539ada76e81b5685203597d6413d4d5e
Zach417 <zach@slaterobots.com>
2018-03-08 22:04:06 -0600

    added more controllers

--- src/tr1_xbox_controller_node.py
+++ src/tr1_xbox_controller_node.py
@@ -4,12 +4,20 @@ import rospy
 from sensor_msgs.msg import Joy
 from std_msgs.msg import Float64
 
-pub = rospy.Publisher('/tr1/arm1_to_arm2_joint_effort_controller/command', Float64, queue_size=10)
+pubs = []
+pubs.append(rospy.Publisher('/tr1/arm1_to_arm2_joint_effort_controller/command', Float64, queue_size=10))
+pubs.append(rospy.Publisher('/tr1/arm2_to_arm3_joint_effort_controller/command', Float64, queue_size=10))
+pubs.append(rospy.Publisher('/tr1/arm3_to_arm4_joint_effort_controller/command', Float64, queue_size=10))
+pubs.append(rospy.Publisher('/tr1/arm4_to_arm5_joint_effort_controller/command', Float64, queue_size=10))
+#pubs.append(rospy.Publisher('/tr1/arm5_to_arm6_joint_effort_controller/command', Float64, queue_size=10))
+#pubs.append(rospy.Publisher('/tr1/arm6_to_arm7_joint_effort_controller/command', Float64, queue_size=10))
+#pubs.append(rospy.Publisher('/tr1/arm7_to_arm8_joint_effort_controller/command', Float64, queue_size=10))
 
 def subscriber_callback(data):
-	joint1_effort = data.axes[0]
-	rospy.loginfo(joint1_effort)
-	pub.publish(joint1_effort)
+	pubs[0].publish(data.axes[0])
+	pubs[1].publish(data.axes[1])
+	pubs[2].publish(data.axes[4])
+	pubs[3].publish(data.axes[3])
 
 def do_control():
 	rospy.init_node('tr1_xbox_controller', anonymous=True)
